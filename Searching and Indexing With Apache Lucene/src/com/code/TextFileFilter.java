a078ebb81c39b3f22575bfe1f4cd6f590d69678f
Jayasurya Muthusamy <jayasurya.muthusamy@impigertech.com>
2019-09-10 10:34:03 +0530

    Apache Lucene work towards documents indexing and searching

---
+++ Searching and Indexing With Apache Lucene/src/com/code/TextFileFilter.java
@@ -0,0 +1,9 @@
+package com.code;
+import java.io.File;
+import java.io.FileFilter;
+
+public class TextFileFilter implements FileFilter {
+	@Override
+	public boolean accept(File pathname) {
+	return pathname.getName().toLowerCase().endsWith(".txt"); }
+}
